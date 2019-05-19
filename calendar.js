f0f1076b1fc05df7b9b62432e06ea8ef81649750
vagrant <vagrant@localhost.localdomain>
2019-05-19 21:11:23 +0900

    カレンダーをクリックした時の処理を追加

--- calendar.js
+++ calendar.js
@@ -1,55 +1,112 @@
 'use strict';
 
+const container = document.getElementById('c-container');
 const calendar = document.getElementById('calendar');
+const candidate = document.getElementById('candidate');
 
 const weeks = ['日', '月', '火', '水', '木', '金', '土'];
 
+let candidateArray = new Array();
+
 const today = new Date();
 
-const year = today.getFullYear();
-const month = today.getMonth();
+let year = today.getFullYear();
+let month = today.getMonth();
 
-const monthBegin = new Date(year, month, 1);
-const monthEnd = new Date(year, month + 1, 0);
+let html = '<i id="prev" class="fas fa-chevron-circle-left"></i>';
+html += '<i id="next" class="fas fa-chevron-circle-right"></i>';
 
-//はじめの曜日
-const dayBegin = monthBegin.getDay();
-//さいごの日
-const lastDay = monthEnd.getDate();
+container.insertAdjacentHTML('afterbegin', html);
 
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
 
+prev.addEventListener('click', () => {
+  document.removeEventListener('click', add_data_attribute);
+  if (month > 0) {
+    month--;
+  } else {
+    month = 11;
+    year--;
+  }
+  show_calendar(year, month);
+});
 
-let day = 1;
+next.addEventListener('click', () => {
+  document.removeEventListener('click', add_data_attribute);
+  if (month < 11) {
+    month++;
+  } else {
+    year++;
+    month = 0;
+  }
+  show_calendar(year, month);
+});
 
-let html = '<h2>' + year + '/' + (month + 1) + '</h2>';
+show_calendar(year, month);
 
-html += '<table><tr>';
+function add_data_attribute(e) {
+  if (e.target.classList.contains('able-day')) {
+    const data_date = e.target.dataset.date;
+    let candidate_date = new Date(data_date);
 
-for (let i = 0; i < weeks.length; i++) {
-  html += '<td>' + weeks[i] + '</td>';
+    //すでに候補日に追加されてる場合は追加しない。
+    if (candidateArray.includes(data_date)) {
+      return;
+    }
+    //候補日の配列に追加
+    candidateArray.push(data_date);
+    //日付を候補日に追加
+    candidate.innerHTML += `${candidate_date.getMonth() + 1}/${candidate_date.getDate()}(${weeks[candidate_date.getDay()]})\n`;
+  }
 }
 
-html += '</tr>';
+function show_calendar(year, month) {
+  const monthBegin = new Date(year, month, 1);
+  const monthEnd = new Date(year, month + 1, 0);
 
-for (let w = 0; w < 6; w++) {
+  //はじめの曜日
+  const dayBegin = monthBegin.getDay();
+  //さいごの日
+  const dayEnd = monthEnd.getDate();
+  const lastMonthDayEnd = (new Date(year, month, 0)).getDate();
 
-  html += '<tr>';
-  
-  for (let d = 0; d < 7; d++) {
-    
-    if (w === 0 && d < dayBegin) {
-      html += '<td></td>';
-    } else if (day <= lastDay) {
-      html += '<td>' + day + '</td>';
-      day++;
-    } else {
-      html += '<td></td>';
-    }
+  let day = 1;
+
+  let html = '<h2>' + year + '/' + (month + 1) + '</h2>';
+
+  html += '<table><tr>';
+
+  for (let i = 0; i < weeks.length; i++) {
+    html += '<td>' + weeks[i] + '</td>';
   }
 
   html += '</tr>';
+
+  for (let w = 0; w < 6; w++) {
+
+    html += '<tr>';
+    
+    for (let d = 0; d < 7; d++) {
+      
+      if (w === 0 && d < dayBegin) {
+        html += `<td class="disable">${lastMonthDayEnd - dayBegin + d + 1}</td>`;
+      } else if (day <= dayEnd) {
+        html += `<td class="able-day" data-date=${year}-${month + 1}-${day}>${day}</td>`;
+        day++;
+      } else {
+        html += `<td class="disable">${day - dayEnd}</td>`;
+        day++;
+      }
+    }
+
+    html += '</tr>';
+  }
+
+  html += '</table>';
+
+  calendar.innerHTML = html;
+
+  document.addEventListener("click", add_data_attribute);
+
 }
-
-html += '</table>';
-
-calendar.innerHTML = html;
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
   <title>Scheduler</title>
 </head>
 <body>
@@ -17,14 +18,16 @@
       </div>
       <div>
         <p>候補日程</p>
-        <textarea name="candidate"></textarea>
+        <textarea id="candidate" name="candidate" rows=10></textarea>
       </div>
       <div>
         <input type="submit" value="予定をつくる">
       </div>
     </form>
-    <div id="calendar">
-        <!-- js -->
+    <!-- js -->
+    <div id="c-container">
+      <div id="calendar">
+      </div>
     </div>
   </div>
   <script src="calendar.js"></script>
